7b38282ce90665bfb5966d657814dbce957bc3b6
Martin Kumst <martin.kumst@seznam.cz>
2017-02-02 15:47:04 +0100

    SQLite connector file renamed, added loader

---
+++ src/LiteORM.php
@@ -0,0 +1,10 @@
+<?php
+
+// Load all files
+
+require_once "LiteORMException.php";
+require_once "LiteORMBaseConnector.php";
+require_once "LiteORMSQLiteConnector.php";
+require_once "LiteORMConnector.php";
+require_once "LiteORMDataObject.php";
+
