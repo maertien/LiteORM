53e8b98b01a735d32e68f21f0458cd7561b25f11
Martin Kumst <martin.kumst@forpsi.com>
2017-11-14 15:34:05 +0100

    Removed old liteorm data object

--- src/LiteORM.php
+++ src/LiteORM.php
@@ -5,7 +5,6 @@ require_once dirname(__FILE__) . "/LiteORMException.php";
 require_once dirname(__FILE__) . "/connectors/LiteORMBaseConnector.php";
 require_once dirname(__FILE__) . "/connectors/LiteORMSQLiteConnector.php";
 require_once dirname(__FILE__) . "/connectors/LiteORMConnector.php";
-require_once dirname(__FILE__) . "/LiteORMDataObject.php";
 require_once dirname(__FILE__) . "/LiteORMReflector.php";
 require_once dirname(__FILE__) . "/LiteORMValidationHelper.php";
 require_once dirname(__FILE__) . "/LiteORMEntityManager.php";
