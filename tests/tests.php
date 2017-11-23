241ca487b6c013d9c59c7bda9cc3decfbdc6de61
Martin Kumst <martin.kumst@forpsi.com>
2017-11-23 11:39:23 +0100

    Tests improved

--- tests/tests.php
+++ tests/tests.php
@@ -34,18 +34,56 @@ try {
 	$em = new LiteORMEntityManager();
 	$e = new E();
 	$e->setA("AAA");
-	$em->save($e);
-	echo "New E entity should be saved\n";
+	$newID = $em->save($e);
+	echo "New E entity should be saved - new id is " . $newID . "\n";
 }
 catch (Exception $e) {
 	echo "ERROR: " . $e->getMessage() . "\n";
 }
 
-
-// Insert into database
-
 // Load entity from database
+try {
+	$em = new LiteORMEntityManager();
+	$e = $em->find("E", $newID);
+	echo "Entity with id " . $newID . " should be loaded\n";
+	$aVal = $e->getA();
+	if ($aVal === "AAA") {
+		echo "A value is ok\n";
+	}
+	else {
+		echo "A value is NOT ok\n";
+	}
+}
+catch (Exception $e) {
+	echo "ERROR: " . $e->getMessage() . "\n";
+}
 
 // Update entity
+try {
+	$em = new LiteORMEntityManager();
+	$e->setA("BBB");
+	$em->save($e);
+	echo "Entity should be updated and will be checked A value\n";
+
+	$ee = $em->find("E", $newID);
+	$nVal = $ee->getA();
+	if ($nVal === "BBB") {
+		echo "New A value is ok\n";
+	}
+	else {
+		echo "New A value is NOT ok\n";
+	}	
+}
+catch (Exception $e) {
+	echo "ERROR: " . $e->getMessage() . "\n";
+}
 
 // Delete entity
+try {
+	$em = new LiteORMEntityManager();
+	$em->delete($ee);
+	echo "Entity should be deleted\n";	
+}
+catch (Exception $e) {
+	echo "ERROR: " . $e->getMessage() . "\n";
+}
